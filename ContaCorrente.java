6e34b74fb2ddd106ed47a30b76cedfb3dcdbfb40
Luigi-Okano <luigitorlai025@gmail.com>
2025-06-18 09:26:39 -0300

    Create ContaCorrente.java

---
+++ ContaCorrente.java
@@ -0,0 +1,9 @@
+package A3_Banco;
+
+public class ContaCorrente extends Conta {
+    private static final long serialVersionUID = 1L;
+
+    public ContaCorrente(String numero, String titular) {
+        super(numero, titular);
+    }
+}
