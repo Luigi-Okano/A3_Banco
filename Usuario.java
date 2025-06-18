2ea02d6dee2d33baff619871001e4efeb7372c80
Luigi-Okano <luigitorlai025@gmail.com>
2025-06-18 09:29:59 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,34 @@
+package A3_Banco;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private String senha;
+    private List<Conta> contas = new ArrayList<>();
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+}
