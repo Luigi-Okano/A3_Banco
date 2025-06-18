2771efb26748b5c55e92f309e944a5e7f3300b04
Luigi-Okano <luigitorlai025@gmail.com>
2025-06-18 09:27:07 -0300

    Create ContaPoupanca.java

---
+++ ContaPoupanca.java
@@ -0,0 +1,9 @@
+package A3_Banco;
+
+public class ContaPoupanca extends Conta {
+    private static final long serialVersionUID = 1L;
+
+    public ContaPoupanca(String numero, String titular) {
+        super(numero, titular);
+    }
+}
